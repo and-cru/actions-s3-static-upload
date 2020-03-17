f8e4fea15e47dfb4f0b531d24710248171c7e1bc
pedreviljoen <pedre87@gmail.com>
2020-03-17 08:06:17 +0200

    Modified s3 upload function

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -3,7 +3,6 @@ import { exec } from "@actions/exec";
 export async function syncToS3Bucket({
   localSource,
   s3Bucket,
-  filesNotToBrowserCache,
   browserCacheDuration,
   cdnCacheDuration
 }) {
@@ -13,42 +12,14 @@ export async function syncToS3Bucket({
     browserCacheDuration,
     cdnCacheDuration
   );
-
-  await setNoBrowserCaching(s3Bucket, filesNotToBrowserCache, cdnCacheDuration);
 }
 
 async function syncEverythingWithBrowserCaching(
   source = "",
-  s3Bucket = "",
-  browserCacheDuration = 0,
-  cdnCacheDuration = 0
+  s3Bucket = ""
 ) {
-  const browserCachingHeader = getCacheControlHeader(
-    browserCacheDuration,
-    cdnCacheDuration
-  );
-
   await exec(
-    `aws s3 sync ${source} s3://${s3Bucket} \
-        --delete \
-        --cache-control "${browserCachingHeader}"`
-  );
-}
-
-async function setNoBrowserCaching(
-  s3Bucket = "",
-  filePatterns = [],
-  cdnCacheDuration = 0
-) {
-  const noBrowserCachingHeader = getCacheControlHeader(0, cdnCacheDuration);
-
-  await exec(
-    `aws s3 cp s3://${s3Bucket} s3://${s3Bucket} \
-        --exclude "*" \
-        ${filePatterns.map(pattern => `--include "${pattern}"`).join(" ")}
-        --recursive \
-        --metadata-directive REPLACE \
-        --cache-control "${noBrowserCachingHeader}"`
+    `aws s3 sync ${source} s3://${s3Bucket}`
   );
 }
 
