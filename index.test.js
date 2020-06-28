184b2c4c37b3fe12eab22f955d6051558fa91617
pedreviljoen <pedre87@gmail.com>
2020-06-28 16:52:39 +0200

    Added milliseconds input

--- index.test.js
+++ index.test.js
@@ -1,7 +1,4 @@
 const wait = require('./wait')
-const process = require('process')
-const cp = require('child_process')
-const path = require('path')
 
 test('throws invalid number', async() => {
     await expect(wait('foo')).rejects.toThrow('milleseconds not a number');
