beaac123e9a1cc72b145ab5a32568fbf50ac8cd7
pedreviljoen <pedre87@gmail.com>
2020-06-28 12:30:15 +0200

    Some cleanup

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -2,40 +2,9 @@ import { exec } from "@actions/exec";
 
 export async function syncToS3Bucket({
   localSource,
-  s3Bucket,
-  browserCacheDuration,
-  cdnCacheDuration
+  s3Bucket
 }) {
-  await syncEverythingWithBrowserCaching(
-    localSource,
-    s3Bucket,
-    browserCacheDuration,
-    cdnCacheDuration
-  );
-}
-
-async function syncEverythingWithBrowserCaching(
-  source = "",
-  s3Bucket = ""
-) {
   await exec(
-    `aws s3 sync ${source} s3://${s3Bucket}`
+    `aws s3 sync ${localSource} s3://${s3Bucket}`
   );
-}
-
-export function getCacheControlHeader(
-  browserCacheDuration = 0,
-  cdnCacheDuration = 0
-) {
-  let header = "";
-
-  if (browserCacheDuration > 0) {
-    header += `public, max-age=${browserCacheDuration}, immutable`;
-  } else {
-    header += "public, max-age=0, must-revalidate";
-  }
-
-  header += `, s-maxage=${cdnCacheDuration}`;
-
-  return header;
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import {getInput, setFailed} from '@actions/core'
-import {getIntInput} from './input'
 import {syncToS3Bucket} from './aws/s3'
 
 async function deploy() {
     await syncToS3Bucket({
-        localSource: 'build/',
-        s3Bucket: getInput('s3-bucket-name', {required: true}),
-        filesNotToBrowserCache: ['*.html'],
-        browserCacheDuration: getIntInput('browser-cache-duration'),
-        cdnCacheDuration: getIntInput('cdn-cache-duration')
+        localSource: './build/',
+        s3Bucket: getInput('s3-bucket-name', {required: true})
     })
 }
 
