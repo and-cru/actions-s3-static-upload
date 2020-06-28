03ecf8ab1a6e6e48c10eddfab036d05f7f1c34d8
pedreviljoen <pedre87@gmail.com>
2020-06-28 17:08:02 +0200

    Structure changes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const {core} = require('@actions/core')
-const {wait} = require('./wait')
-const {runDeploy} = require('./src/main')
+const { core } = require('@actions/core')
+const { runDeploy } = require('./main')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try{
-    const ms = core.getInput('milliseconds')
-    await wait(ms)
     core.info('Starting upload')
     await runDeploy()
     core.info('Finished upload')
